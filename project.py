642cefb799c36d8bd7bc4f3a3ef4fac1b4aa41f3
Mina C <mina.choe@outlook.com>
2019-07-31 05:57:11 +0900

    fix: random error when restaurant id on route changes

--- project.py
+++ project.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
@@ -33,4 +33,3 @@ def restaurantMenu(restaurant_id):
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-    
