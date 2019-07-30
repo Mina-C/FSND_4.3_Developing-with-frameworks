1a114f1eb95daf07a5a9efbe5af403155e8b39a7
Mina C <mina.choe@outlook.com>
2019-07-31 05:53:53 +0900

    feat: menu will be shown by restaurant id on route

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 
 
@@ -14,10 +15,9 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
@@ -27,9 +27,10 @@ def HelloWorld():
         output += '</br>'
         output += i.description
         output += '</br>'
-    output += '</br>'
+        output += '</br>'
     return output
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+    
