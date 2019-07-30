b35c2855f4725db68b7a796ee09ad0adc78b0ff5
Mina C <mina.choe@outlook.com>
2019-07-30 19:36:26 +0900

    feat: import sqlalchemy, sessionmaker and db

--- project.py
+++ project.py
@@ -3,6 +3,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
